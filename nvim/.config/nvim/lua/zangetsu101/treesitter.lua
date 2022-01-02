3ec47791353a6b7eda1918d359a4ec0856bb62e7
Tameem Bin Haider <haidertameem@gmail.com>
2022-01-02 10:02:43 +0600

    Added default tree-sitter modules

--- nvim/.config/nvim/lua/zangetsu101/treesitter.lua
+++ nvim/.config/nvim/lua/zangetsu101/treesitter.lua
@@ -1,17 +1,9 @@
 local treesitter = require('nvim-treesitter.configs')
 
 treesitter.setup {
+  ensure_installed = {'c', 'cpp', 'css','dockerfile', 'graphql', 'jsonc', 'tsx', 'typescript'},
   highlight = {
     enable = true
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = 'gnn',
-      node_incremental = 'grn',
-      scope_incremental = 'grc',
-      node_decremental = 'grm'
-    }
   },
   indent = {
     enable = true
