7daa19d9d62b3860bded5ca69e93e8a861cd7a95
Tameem Bin Haider <haidertameem@gmail.com>
2022-01-18 11:45:57 +0600

    Separated nvim-cmp from lspconfig

--- nvim/.config/nvim/lua/zangetsu101/lsp.lua
+++ nvim/.config/nvim/lua/zangetsu101/lsp.lua
@@ -67,31 +67,3 @@ lsp_installer.on_server_ready(function(server)
 
   server:setup(opts)
 end)
-
-local cmp = require('cmp')
-local lspkind = require('lspkind')
-local luasnip = require('luasnip')
-
-cmp.setup {
-  completion = {
-    autocomplete = false
-  },
-  mapping = {
-    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' })
-  },
-	-- Format the autocomplete menu
-	formatting = {
-		format = lspkind.cmp_format()
-	},
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  }
-}
