3cb31aa3f74fb262239546cdddd205627c62e79d
Tameem Bin Haider <haidertameem@gmail.com>
2022-01-23 15:52:54 +0600

    Improved the fileMatch for tsconfig schema

--- nvim/.config/nvim/lua/zangetsu101/lsp.lua
+++ nvim/.config/nvim/lua/zangetsu101/lsp.lua
@@ -55,6 +55,14 @@ local enhance_server_opts = {
             'prettierrc.json',
             'tsconfig.json',
             'tslint.json'
+          },
+          replace = {
+            ['tsconfig.json'] = {
+              description = 'JSON schema for typescript configuration files',
+              fileMatch = {'tsconfig*.json'},
+              name = 'tsconfig.json',
+              url = 'https://json.schemastore.org/tsconfig.json'
+            }
           }
         }
       }
