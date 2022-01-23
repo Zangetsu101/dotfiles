02e2cec0b9d210c5898c9a53196bd76b32093934
Tameem Bin Haider <haidertameem@gmail.com>
2022-01-23 14:49:04 +0600

    Configured some options for lualine

--- nvim/.config/nvim/lua/zangetsu101/lualine.lua
+++ nvim/.config/nvim/lua/zangetsu101/lualine.lua
@@ -3,5 +3,9 @@ local lualine = require('lualine')
 lualine.setup {
   options = {
     theme = 'gruvbox'
-  }
+  },
+  sections = {
+    lualine_x = {'filetype'}
+  },
+  extensions = {'nvim-tree', 'fugitive', 'quickfix'}
 }
