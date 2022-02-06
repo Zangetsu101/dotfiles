1afb075f42612a55726e7f403b27168d147a4173
Tameem Bin Haider <haidertameem@gmail.com>
2022-02-06 23:45:58 +0600

    Disable indent_blankline for help files

--- nvim/.config/nvim/lua/zangetsu101/indent_blankline.lua
+++ nvim/.config/nvim/lua/zangetsu101/indent_blankline.lua
@@ -3,6 +3,7 @@ local indent_blankline = require('indent_blankline')
 vim.opt.list = true
 vim.opt.listchars:append("eol:↴")
 vim.g.indent_blankline_use_treesitter = true
+vim.g.indent_blankline_filetype_exclude = { 'help' }
 
 indent_blankline.setup {
   show_end_of_line = true,
