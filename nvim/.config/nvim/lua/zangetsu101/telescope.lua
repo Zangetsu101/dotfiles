b4a8f328390f582f167de6fe1c5fdd353348fa7b
Tameem Bin Haider <haidertameem@gmail.com>
2022-05-06 09:59:57 +0600

    Hid telescope preview by default

--- nvim/.config/nvim/lua/zangetsu101/telescope.lua
+++ nvim/.config/nvim/lua/zangetsu101/telescope.lua
@@ -1,8 +1,20 @@
 local telescope = require('telescope')
+local layout_actions = require('telescope.actions.layout')
 
 telescope.setup {
   defaults = {
-    path_display = {'truncate'}
+    path_display = {'truncate'},
+    preview = {
+      hide_on_startup = true
+    },
+    mappings = {
+      n = {
+        ["<C-h>"] = layout_actions.toggle_preview
+      },
+      i = {
+        ["<C-h>"] = layout_actions.toggle_preview
+      }
+    }
   }
 }
 
