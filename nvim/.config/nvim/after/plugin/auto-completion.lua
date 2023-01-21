e201f0d473a5773fff1436dfa6d171d9657cd2fc
Tameem Bin Haider <haidertameem@gmail.com>
2023-01-21 22:15:44 +0600

    Add separate keybindings for luasnip jump

--- nvim/.config/nvim/after/plugin/auto-completion.lua
+++ nvim/.config/nvim/after/plugin/auto-completion.lua
@@ -19,13 +19,17 @@ cmp.setup {
     ['<C-Space>'] = cmp.mapping.complete({}),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<C-y>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
+      select = false,
     },
     ['<C-n>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if luasnip.expand_or_locally_jumpable() then
         luasnip.expand_or_jump()
       else
         fallback()
@@ -34,7 +38,12 @@ cmp.setup {
     ['<C-p>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if luasnip.locally_jumpable(-1) then
         luasnip.jump(-1)
       else
         fallback()
