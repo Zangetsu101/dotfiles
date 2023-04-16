7680643263e109b40e8bceb07a78b8f9cf6e26d0
Tameem Bin Haider <haidertameem@gmail.com>
2023-04-16 12:19:20 +0600

    Change lua server name to "lua_ls"

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -99,7 +99,7 @@ local servers = {
     }
   },
   tsserver = {},
-  sumneko_lua = {
+  lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
       telemetry = { enable = false },
