454ee2604d6ba8b44a7266a5de755f7e9d81f24a
Tameem Bin Haider <haidertameem@gmail.com>
2023-09-30 23:04:34 +0600

    Add treesitter-context

---
+++ nvim/after/plugin/treesitter-context.lua
@@ -0,0 +1,3 @@
+require'treesitter-context'.setup{
+  multiline_threshold = 1, -- Maximum number of lines to show for a single context
+}
--- nvim/lua/zangetsu101/packer.lua
+++ nvim/lua/zangetsu101/packer.lua
@@ -72,5 +72,6 @@ return require('packer').startup(function(use)
 	use 'rstacruz/vim-closer'
 	use 'nvim-telescope/telescope-ui-select.nvim'
 	use 'simrat39/rust-tools.nvim'
-	use("aserowy/tmux.nvim")
+	use 'aserowy/tmux.nvim'
+	use 'nvim-treesitter/nvim-treesitter-context'
 end)
