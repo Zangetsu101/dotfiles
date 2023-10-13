1bf4da209d81b4cbf96dc2a12cfc27ed7407bf0d
Tameem Bin Haider <haidertameem@gmail.com>
2023-10-13 16:04:11 +0600

    Bump up telescope to 0.1.3

--- nvim/lua/zangetsu101/packer.lua
+++ nvim/lua/zangetsu101/packer.lua
@@ -2,7 +2,7 @@ return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 
 	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.0',
+		'nvim-telescope/telescope.nvim', tag = '0.1.3',
 		requires = { { 'nvim-lua/plenary.nvim' } }
 	}
 
