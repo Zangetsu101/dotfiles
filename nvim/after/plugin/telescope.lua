e16c38f6a2ec728f90f8400865e71fad0a2fed39
Tameem Bin Haider <haidertameem@gmail.com>
2023-11-11 17:19:38 +0600

    Add mapping for visual selection search

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -5,12 +5,30 @@ pcall(telescope.load_extension, 'ui-select')
 
 local builtin = require('telescope.builtin')
 
+local function buf_vtext()
+  local a_orig = vim.fn.getreg('a')
+  local mode = vim.fn.mode()
+  if mode ~= 'v' and mode ~= 'V' then
+    vim.cmd([[normal! gv]])
+  end
+  vim.cmd([[silent! normal! "aygv]])
+  local text = vim.fn.getreg('a')
+  vim.fn.setreg('a', a_orig)
+  return text
+end
+
+local function search_vtext()
+  local vtext = buf_vtext()
+  builtin.grep_string({ search = vtext })
+end
+
 vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = '[/] Fuzzily search in current buffer]' })
 vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('v', '<leader>sw', search_vtext, { desc = '[S]earch selection' })
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
