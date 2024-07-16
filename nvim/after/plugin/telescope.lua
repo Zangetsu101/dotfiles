954ca101893b9e763ce9cb9b4f5d7f015a2f853f
Tameem Bin Haider <haidertameem@gmail.com>
2024-07-16 10:55:41 +0600

    feat: search through hidden files with telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -8,6 +8,15 @@ local actions = require('telescope.actions')
 
 telescope.setup {
   pickers = {
+    find_files = {
+      hidden = true
+    },
+    grep_string = {
+      additional_args = {"--hidden"}
+    },
+    live_grep = {
+      additional_args = {"--hidden"}
+    },
     buffers = {
       mappings = {
         n = {
@@ -38,16 +47,16 @@ end
 vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = '[/] Fuzzily search in current buffer]' })
-vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sf', function() builtin.find_files({ hidden = true }) end, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('v', '<leader>sw', search_vtext, { desc = '[S]earch selection' })
-vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sg', function() builtin.live_grep({ hidden = true }) end, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
 
 vim.api.nvim_create_user_command('Rg', function(opts)
-    builtin.grep_string({ search = opts.fargs[1] })
-  end,
+  builtin.grep_string({ search = opts.fargs[1] })
+end,
   { nargs = 1 }
 )
