9c2903fa290cdcba61e89eb56b6db7932fb4a329
Tameem Bin Haider <haidertameem@gmail.com>
2025-11-20 10:51:31 +0600

    feat: update telescope to v0.1.9

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
   'nvim-telescope/telescope.nvim',
   event = 'VimEnter',
-  branch = '0.1.x',
+  tag = 'v0.1.9',
   dependencies = {
     'nvim-lua/plenary.nvim',
     { -- If encountering errors, see telescope-fzf-native README for installation instructions
