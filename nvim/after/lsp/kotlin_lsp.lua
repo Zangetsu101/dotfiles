a0a1f691856e2325d8d2e8a907b23bc3f7bda6fb
Tameem Bin Haider <haidertameem@gmail.com>
2026-05-22 00:24:24 +0600

    fix: overwrite the default kotlin lsp config

--- nvim/after/lsp/kotlin_lsp.lua
+++ nvim/after/lsp/kotlin_lsp.lua
@@ -1,16 +1,3 @@
-local lspconfig = require 'lspconfig'
-
 return {
-  default_config = {
-    cmd = { '/home/tameem/.cargo/bin/kotlin-lsp' },
-    filetypes = { 'kotlin', 'java', 'swift' },
-    root_dir = lspconfig.util.root_pattern(
-      'build.gradle',
-      'build.gradle.kts',
-      'pom.xml',
-      'settings.gradle',
-      'Package.swift',
-      '.git'
-    ),
-  }
+  cmd = { "intellij-server", "--stdio" }
 }
